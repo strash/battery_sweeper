65b28118881bc2def00d984c10b79e340320bbe8
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-02-07 01:05:03 +0500

    Initial Commit

---
+++ BatterySweeper/BatterySweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatterySweeperApp.swift
+//  BatterySweeper
+//
+//  Created by Dmitry Poyarkov on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BatterySweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatterySweeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BatterySweeper
+//
+//  Created by Dmitry Poyarkov on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
