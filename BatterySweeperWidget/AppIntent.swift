52d38d05f1c3f017a46f018af529d57504cff079
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-02-13 00:56:13 +0300

    feat: widget

---
+++ BatterySweeperWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  BatterySweeperWidget
+//
+//  Created by Dmitry Poyarkov on 2/13/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ BatterySweeperWidget/BatterySweeperWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BatterySweeperWidgetBundle.swift
+//  BatterySweeperWidget
+//
+//  Created by Dmitry Poyarkov on 2/13/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BatterySweeperWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BatterySweeperWidget()
+    }
+}
